6c32504260ce1815fe9de16f37d65ce8d687d139
LuHu <luhu0105@gmail.com>
2017-06-01 11:05:49 +0800

    add 卢虎 （4） 17.06.01，实现较复杂的业务

--- singleTest/single.py
+++ singleTest/single.py
@@ -1,37 +1,43 @@
-# __author__=luhu
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf_8 -*-
 import requests
 import xlrd
 
 
-data = xlrd.open_workbook('\API_source\API.xlsx')
+data = xlrd.open_workbook('D:\JetBrains\PythonProject\APITest\API_source\API.xlsx', 'utf-8')
 table = data.sheets()[0]  # 该表的第1个页签
 nrows = table.nrows  # 获取表的行数
 # print nrows
-for i in range(1, nrows):
-    # print table.cell_value(0, 0)   # 第1行，第一列
-    method = table.cell_value(i, 3)
-    url = table.cell_value(i, 0)
-    data = table.cell_value(i, 2)
-    headers = table.cell_value(i, 1)
-    if data:
-        data = eval(data)
+# for i in range(1, nrows):
+# print table.cell_value(0, 0)   # 第1行，第一列
+method = table.cell_value(60, 3)
+url = table.cell_value(60, 0)
+data = table.cell_value(60, 2)
+headers = table.cell_value(60, 1)
+if data:
+    data = eval(data)
 
-    if method == 'post':
-        if not headers:
-            r = requests.post(url=url, json=data)
-        else:
-            headers = eval(headers)
-            r = requests.post(url=url, json=data, headers=headers)
-    elif method == 'get':
-        headers = eval(headers)
-        r = requests.get(url=url, headers=headers)
-    elif method == 'put':
-        headers = eval(headers)
-        r = requests.put(url=url, json=data, headers=headers)
-    elif method == 'delete':
-        headers = eval(headers)
-        r = requests.delete(url=url, headers=headers)
+if method == 'post':
+    if headers == '':
+        requests.session().keep_alive = False
+        r = requests.post(url=url, json=data)
 
-    print str(url)+'    响应码：' + str(r.status_code)
-    print r.text
+    else:
+        headers = eval(headers)
+        requests.session().keep_alive = False
+        r = requests.post(url=url, json=data, headers=headers)
+
+elif method == 'get':
+    headers = eval(headers)
+    r = requests.get(url=url, headers=headers)
+
+elif method == 'put':
+    headers = eval(headers)
+    r = requests.put(url=url, json=data, headers=headers)
+
+elif method == 'delete':
+    headers = eval(headers)
+    r = requests.delete(url=url, headers=headers)
+
+print str(url)+'    响应码：' + str(r.status_code)
+print r.text
--- testCase/test_request.py
+++ testCase/test_request.py
@@ -24,7 +24,7 @@ class test_userAPI(MyTest):    # 将单个接口封装成一个类，其中的�
         table = data.sheets()[0]   # 该表的第1个页签
         nrows = table.nrows   # 获取表的行数
         # print nrows
-        for i in range(1, nrows):
+        for i in range(31, 33):
             # print table.cell_value(0, 0)   # 第1行，第一列
             method = table.cell_value(i, 3)
             url = table.cell_value(i, 0)
@@ -35,7 +35,7 @@ class test_userAPI(MyTest):    # 将单个接口封装成一个类，其中的�
                 self.data = eval(data)
 
             if method == 'post':
-                if headers == "":
+                if headers == '':
                     self.r = requests.post(url=self.url, json=self.data)
                 else:
                     self.r = requests.post(url=self.url, json=self.data, headers=eval(headers))
